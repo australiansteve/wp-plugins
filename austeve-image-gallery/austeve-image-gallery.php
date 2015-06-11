56f391c5e1fb6fb667022099962a62a7a063945d
australiansteve <steve@australiansteve.com>
2015-06-10 22:44:17 -0300

    Add widget to plugin so that multiple 'galleries' can be previewed, with the help of amr-shortcode-any-widget, although that is not required

--- austeve-image-gallery/austeve-image-gallery.php
+++ austeve-image-gallery/austeve-image-gallery.php
@@ -12,6 +12,7 @@
 function austeve_image_gallery_styles() {
     wp_enqueue_style( 'slick_styles', plugin_dir_url( __FILE__ ) . 'bower_components/slick.js/slick/slick.css', array() );
     wp_enqueue_style( 'slick_theme_styles', plugin_dir_url( __FILE__ ) . 'bower_components/slick.js/slick/slick-theme.css', array() );
+    wp_enqueue_style( 'austeve_image_gallery_preview_styles', plugin_dir_url( __FILE__ ) . 'style.css', array() );
 }
 add_action( 'wp_enqueue_scripts', 'austeve_image_gallery_styles', 11 );
 
@@ -98,4 +99,116 @@ function add_init_scripts() {
 }
 add_action('init', 'add_init_scripts');
 
+
+// Creating the widget 
+class austeve_gallery_widget extends WP_Widget {
+
+    function __construct() {
+        parent::__construct(
+        // Base ID of your widget
+        'austeve_gallery_widget', 
+
+        // Widget name will appear in UI
+        __('AUSteve Gallery Widget', 'austeve_gallery_widget_domain'), 
+
+        // Widget description
+        array( 'description' => __( 'Preview for a gallery', 'austeve_gallery_widget_domain' ), ) 
+        );
+    }
+
+    // Creating widget front-end
+    // This is where the action happens
+    public function widget( $args, $instance ) {
+        $title = apply_filters( 'widget_title', $instance['title'] );
+        // before and after widget arguments are defined by themes
+        echo $args['before_widget'];
+
+        // This is where you run the code and display the output
+        $widgetOutput = "<div class='container'>";
+        $widgetOutput .= "<div class='layover'><h2 class='title'>".$instance['title']."</h2>";
+        if (isset($instance['description'])) {
+            $widgetOutput .= "<div class='description'>".$instance['description']."</div>";
+        }
+        if (isset($instance['action_url'])) {
+            $widgetOutput .= "<a href='".$instance['action_url']."' class='button'>".$instance['action_verb']."</a>";
+        }
+        $widgetOutput .= "</div>"; //div.layover
+        $widgetOutput .= "<div class='img'><img src='".$instance['preview_image']."'/></div>";
+        $widgetOutput .= "</div>"; //div.container
+
+        echo __( $widgetOutput, 'austeve_gallery_widget_domain' );
+        echo $args['after_widget'];
+    }
+        
+    // Widget Backend 
+    public function form( $instance ) {
+        if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+        }
+        else {
+            $title = __( 'Gallery title', 'austeve_gallery_widget_domain' );
+        }
+
+        if ( isset( $instance[ 'preview_image' ] ) ) {
+            $preview_image = $instance[ 'preview_image' ];
+        }
+        else {
+            $preview_image = __( '%image url%', 'austeve_gallery_widget_domain' );
+        }
+        
+        if ( isset( $instance[ 'description' ] ) ) {
+            $description = $instance[ 'description' ];
+        }
+        else {
+            $description = __( '', 'austeve_gallery_widget_domain' );
+        }
+        
+        if ( isset( $instance[ 'action_url' ] ) ) {
+            $action_url = $instance[ 'action_url' ];
+        }
+        else {
+            $action_url = __( '', 'austeve_gallery_widget_domain' );
+        }
+        
+        if ( isset( $instance[ 'action_verb' ] ) ) {
+            $action_verb = $instance[ 'action_verb' ];
+        }
+        else {
+            $action_verb = __( 'Text to show on link button', 'austeve_gallery_widget_domain' );
+        }
+
+        // Widget admin form
+?>
+        <p>
+        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+        <label for="<?php echo $this->get_field_id( 'preview_image' ); ?>"><?php _e( 'Preview image:' ); ?></label> 
+        <input class="widefat" id="<?php echo $this->get_field_id( 'preview_image' ); ?>" name="<?php echo $this->get_field_name( 'preview_image' ); ?>" type="text" value="<?php echo esc_attr( $preview_image ); ?>" />
+        <label for="<?php echo $this->get_field_id( 'description' ); ?>"><?php _e( 'Short description:' ); ?></label> 
+        <input class="widefat" id="<?php echo $this->get_field_id( 'description' ); ?>" name="<?php echo $this->get_field_name( 'description' ); ?>" type="text" value="<?php echo esc_attr( $description ); ?>" />
+        <label for="<?php echo $this->get_field_id( 'action_url' ); ?>"><?php _e( 'Link URL:' ); ?></label> 
+        <input class="widefat" id="<?php echo $this->get_field_id( 'action_url' ); ?>" name="<?php echo $this->get_field_name( 'action_url' ); ?>" type="text" value="<?php echo esc_attr( $action_url ); ?>" />
+        <label for="<?php echo $this->get_field_id( 'action_verb' ); ?>"><?php _e( 'Link wording:' ); ?></label> 
+        <input class="widefat" id="<?php echo $this->get_field_id( 'action_verb' ); ?>" name="<?php echo $this->get_field_name( 'action_verb' ); ?>" type="text" value="<?php echo esc_attr( $action_verb ); ?>" />
+        </p>
+<?php 
+    }
+        
+    // Updating widget replacing old instances with new
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        $instance['preview_image'] = ( ! empty( $new_instance['preview_image'] ) ) ? strip_tags( $new_instance['preview_image'] ) : '';
+        $instance['description'] = ( ! empty( $new_instance['description'] ) ) ? strip_tags( $new_instance['description'] ) : '';
+        $instance['action_url'] = ( ! empty( $new_instance['action_url'] ) ) ? strip_tags( $new_instance['action_url'] ) : '';
+        $instance['action_verb'] = ( ! empty( $new_instance['action_verb'] ) ) ? strip_tags( $new_instance['action_verb'] ) : 'Details';
+        return $instance;
+    }
+} // Class austeve_gallery_widget ends here
+
+// Register and load the widget
+function austeve_gallery_load_widget() {
+    register_widget( 'austeve_gallery_widget' );
+}
+add_action( 'widgets_init', 'austeve_gallery_load_widget' );
 ?>
--- austeve-image-gallery/js/init.js
+++ austeve-image-gallery/js/init.js
@@ -13,3 +13,13 @@ function slickInit() {
 		adaptiveHeight: true
 	});
 }
+
+jQuery(document).on('mouseover', ".widget_austeve_gallery_widget", function() {
+    jQuery(this).find(".layover").show(); 
+    jQuery(this).find(".img").css('opacity', '0.5');     
+});
+
+jQuery(document).on('mouseout', ".widget_austeve_gallery_widget", function() {
+    jQuery(this).find(".layover").hide();   
+    jQuery(this).find(".img").css('opacity', '1');   
+});
